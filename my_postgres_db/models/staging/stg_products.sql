57dde4684fb019592a6934677938a9253132aca9
AbdurRahim92 <abdurrahim@seekasia.com>
2024-05-23 23:02:01 +0800

    add dwh

--- my_postgres_db/models/staging/stg_products.sql
+++ my_postgres_db/models/staging/stg_products.sql
@@ -2,7 +2,22 @@
 -- Table `stg_northwind`.`stg_products`
 -- -----------------------------------------------------
 WITH source AS (
-    SELECT * FROM {{ source('northwind', 'products')}}
+    SELECT CAST(supplier_ids AS INTEGER) AS supplier_id,
+    id,
+    product_code,
+    product_name,
+    description,
+    standard_cost,
+    list_price,
+    reorder_level,
+    target_level,
+    quantity_per_unit,
+    discontinued,
+    minimum_reorder_quantity,
+    category,
+    attachments
+    FROM {{ source('northwind', 'products')}}
+    WHERE supplier_ids NOT LIKE '%;%'
 )
 SELECT * 
 , CURRENT_TIMESTAMP AS ingestion_timestamp
